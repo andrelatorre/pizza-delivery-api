65cab626873c1fc7972881e6f3caceb3ef15414a
Latorre <andre.latorre91@gmail.com>
2026-01-21 18:21:47 -0300

    Creating the first application routes

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from order_routes import order_router
 
 app = FastAPI()
 
+#apos criar os roteadores de rota precisa incluir eles
 app.include_router(auth_router)
 app.include_router(order_router)
+
+
 #para rodar nosso codigo,  executar no terminal: uvicorn main:app --reload
 
 #Rotes
--- order_routes.py
+++ order_routes.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
+
 order_router = APIRouter(prefix="/order", tags=["order"])
+
+@order_router.get("/")
+async def orders():
+    return {"message": "Você acessou a rota de pedidos"}
