5b3ac3e74e4473601245209cc5fcbb9224f9d4ac
Latorre <andre.latorre91@gmail.com>
2026-01-22 20:30:13 -0300

    Created user registration route

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,26 @@
 from fastapi import APIRouter
+from models.models import Usuario, db
+from sqlalchemy.orm import sessionmaker
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def auth():
+async def home():
     """
     Essa é a rota padrão de autenticação do nosso sistema.
     """
     return {"message": "Você acessou a rota padrão de autenticação", "autenticado": False}
+
+@auth_router.post("/criar_conta")
+async def criar_conta(email: str, senha: str, nome: str):
+    Session = sessionmaker(bind=db)
+    session = Session()
+    usuario =  session.query(Usuario).filter(Usuario.email==email).first()
+    if usuario:
+        #ja existe um user com esse email
+        return {"message": "Já existe um usuário com esse email"}
+    else:
+        novo_usuario = Usuario(nome, email, senha)
+        session.add(novo_usuario)
+        session.commit()
+        return {"message": "Usuário cadastrado com sucesso"}
