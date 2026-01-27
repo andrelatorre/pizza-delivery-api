2e62bc372265a0c44953fbc2a5a2ae37a9160a1b
Latorre <andre.latorre91@gmail.com>
2026-01-27 18:50:27 -0300

    Created order route

--- order_routes.py
+++ order_routes.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import pegar_sessao
+from database.schemas import PedidoSchema
+from models.models import Pedido
 
 order_router = APIRouter(prefix="/order", tags=["order"])
 
@@ -9,3 +12,10 @@ async def orders():
     Essa é a rota padrão de pedidos do sistema. Todas as rotas dos pedidos precisam de autenticação.
     """
     return {"message": "Você acessou a rota de pedidos"}
+
+@order_router.post("/pedido")
+async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(pegar_sessao)):
+    novo_pedido = Pedido(usuario = pedido_schema.id_usuario)
+    session.add(novo_pedido)
+    session.commit()
+    return {":message": f"Pedido criado com sucesso. Pedido número: {novo_pedido.id}"}
